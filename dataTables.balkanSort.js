ce9ff5145ac7cc2a5b30010f58d72c1d68bb87ab
Jakob Crnogaj <jakob@email.com>
2016-08-19 16:47:44 +0200

    Update dataTables.balkanSort.js

--- dataTables.balkanSort.js
+++ dataTables.balkanSort.js
@@ -3,24 +3,24 @@ function character_substitute(string) {
 	var chars = /[šđžčć]/g;
 	
 	if (first_char.match(chars)) {
-		if (first_char == "š") { first_char = first_char.replace("š", "s"); return first_char; }
-		if (first_char == "ž") { first_char = first_char.replace("ž", "z"); return first_char; }
-		if (first_char == "č") { first_char = first_char.replace("č", "c"); return first_char; }
-		if (first_char == "ć") { first_char = first_char.replace("ć", "c"); return first_char; }
-		if (first_char == "đ") { first_char = first_char.replace("đ", "d"); return first_char; }
+		if (first_char == "š") { first_char = first_char.replace("š", "sx"); return first_char; }
+		if (first_char == "ž") { first_char = first_char.replace("ž", "zx"); return first_char; }
+		if (first_char == "č") { first_char = first_char.replace("č", "cxx"); return first_char; }
+		if (first_char == "ć") { first_char = first_char.replace("ć", "cx"); return first_char; }
+		if (first_char == "đ") { first_char = first_char.replace("đ", "dx"); return first_char; }
 	}
 	
 	return first_char;
 }
 
-jQuery.fn.dataTableExt.oSort['balkan_string-asc']  = function(a,b) {
+jQuery.fn.dataTableExt.oSort['balkan-string-asc']  = function(a,b) {
 	x = character_substitute(a);
 	y = character_substitute(b);
 	
     return ((x < y) ? -1 : ((x > y) ? 1 : 0));
 };
 
-jQuery.fn.dataTableExt.oSort['balkan_string-desc'] = function(a,b) {
+jQuery.fn.dataTableExt.oSort['balkan-string-desc'] = function(a,b) {
 	x = character_substitute(a);
 	y = character_substitute(b);
 	
